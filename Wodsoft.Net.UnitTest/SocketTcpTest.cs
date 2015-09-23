4560d3ca7a97a8fb6adee7af5e84332f6c9160a5
Kation <lwcqq@163.com>
2015-09-24 00:17:35 +0800

    修正提交

---
+++ Wodsoft.Net.UnitTest/SocketTcpTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Wodsoft.Net.Sockets;
+
+namespace Wodsoft.Net.UnitTest
+{
+    [TestClass]
+    public class SocketTcpTest
+    {
+        [TestMethod]
+        public void TcpSendReceiveTest()
+        {            
+            SocketHandler16 socketHandler = new SocketHandler16();
+            SocketTcpListener<byte[], byte[]> listener = new SocketTcpListener<byte[], byte[]>(socketHandler);
+            listener.Port = 7000;
+            listener.AcceptCompleted += listener_AcceptCompleted;
+            listener.Start();
+            Assert.IsTrue(listener.IsStarted);
+
+            SocketTcpClient<byte[], byte[]> client = new SocketTcpClient<byte[], byte[]>(socketHandler);
+            Assert.IsFalse(client.IsConnected);
+            client.DisconnectCompleted += client_DisconnectCompleted;
+            byte[] data = new byte[] { 100, 255, 80, 64 };
+            Assert.IsTrue(client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7000)));
+            Assert.IsTrue(client.IsConnected);
+            client.ReceiveCycle();
+            Assert.IsTrue(client.Send(data));
+
+            listener.Stop();
+            Assert.IsFalse(listener.IsStarted);
+        }
+
+        void client_DisconnectCompleted(object sender, SocketEventArgs e)
+        {
+            ISocket<byte[], byte[]> socket = (ISocket<byte[], byte[]>)sender;
+            socket.DisconnectCompleted -= client_DisconnectCompleted;
+        }
+
+        void listener_AcceptCompleted(object sender, SocketEventArgs<ISocket<byte[], byte[]>> e)
+        {
+            e.Data.ReceiveCompleted += Data_ReceiveCompleted;
+            e.Data.ReceiveCycle();
+        }
+
+        void Data_ReceiveCompleted(object sender, SocketEventArgs<byte[]> e)
+        {
+            Assert.AreEqual(4, e.Data.Length);
+            Assert.AreEqual(100, e.Data[0]);
+            Assert.AreEqual(255, e.Data[1]);
+            Assert.AreEqual(80, e.Data[2]);
+            Assert.AreEqual(64, e.Data[3]);
+            ISocket<byte[], byte[]> socket = (ISocket<byte[], byte[]>)sender;
+            socket.Disconnect();
+            Assert.IsFalse(socket.IsConnected);
+            socket.ReceiveCompleted -= Data_ReceiveCompleted;
+        }
+    }
+}
---
+++ Wodsoft.Net.UnitTest/SocketUdpTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wodsoft.Net.Sockets;
+using System.Net;
+
+namespace Wodsoft.Net.UnitTest
+{
+    [TestClass]
+    public class SocketUdpTest
+    {
+        private SocketUdpHost<byte[], byte[]> host, host2;
+
+        [TestMethod]
+        public void UdpSendReceiveTest()
+        {
+            SocketSourceHandler socketHandler = new SocketSourceHandler();
+            host = new SocketUdpHost<byte[], byte[]>(socketHandler);
+            host.AcceptCompleted += listener_AcceptCompleted;
+            host.Start(new IPEndPoint(IPAddress.Any, 8000));
+            Assert.IsTrue(host.IsStarted);
+
+            host2 = new SocketUdpHost<byte[], byte[]>(socketHandler);
+            Assert.IsFalse(host2.IsStarted);
+            host2.Start();
+            ISocket<byte[], byte[]> client = host2.Open(new IPEndPoint(IPAddress.Loopback, 8000));
+            client.DisconnectCompleted += client_DisconnectCompleted;
+            byte[] data = new byte[] { 100, 255, 80, 64 };
+            Assert.IsTrue(client.IsConnected);
+            Assert.IsTrue(client.Send(data));
+
+        }
+
+        void client_DisconnectCompleted(object sender, SocketEventArgs e)
+        {
+            ISocket<byte[], byte[]> socket = (ISocket<byte[], byte[]>)sender;
+            socket.DisconnectCompleted -= client_DisconnectCompleted;
+        }
+
+        void listener_AcceptCompleted(object sender, SocketEventArgs<ISocket<byte[], byte[]>> e)
+        {
+            e.Data.ReceiveCompleted += Data_ReceiveCompleted;
+        }
+
+        void Data_ReceiveCompleted(object sender, SocketEventArgs<byte[]> e)
+        {
+            Assert.AreEqual(4, e.Data.Length);
+            Assert.AreEqual(100, e.Data[0]);
+            Assert.AreEqual(255, e.Data[1]);
+            Assert.AreEqual(80, e.Data[2]);
+            Assert.AreEqual(64, e.Data[3]);
+            ISocket<byte[], byte[]> socket = (ISocket<byte[], byte[]>)sender;
+            socket.Disconnect();
+            Assert.IsFalse(socket.IsConnected);
+            socket.ReceiveCompleted -= Data_ReceiveCompleted;
+            host.Stop();
+            host2.Stop();
+            Assert.IsFalse(host.IsStarted);
+        }
+    }
+}
