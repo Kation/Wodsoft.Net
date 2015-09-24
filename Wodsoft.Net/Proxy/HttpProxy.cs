1e9135c244e88f3b8491aa03a577796975f288cd
Kation <lwcqq@163.com>
2015-09-24 18:34:54 +0800

    update

---
+++ Wodsoft.Net/Proxy/HttpProxy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wodsoft.Net.Proxy
+{
+    public class HttpProxy
+    {
+        private int _Port;
+        public int Port
+        {
+            get
+            {
+                return _Port;
+            }
+            set
+            {
+                if (IsStarted)
+                    throw new InvalidOperationException("已经开始服务后不能修改端口。");
+            }
+        }
+
+        public bool IsStarted { get; private set; }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
